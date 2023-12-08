e41a61a7e5cde349fefb6cce90fff02ab4803797
yinpeng <y960303802@163.com>
2023-12-08 17:54:31 +0800

    feat: proxy

---
+++ Chapter9_爬取微信公众号文章/ProxyPool_代理池的维护/proxy_demo.py
@@ -0,0 +1,52 @@
+from bisect import bisect_left, bisect_right, insort_left, insort_right, insort, bisect
+from math import ceil, floor, pow, gcd, sqrt, log10, fabs, fmod, factorial, inf, pi, e
+from heapq import heapify, heapreplace, heappush, heappop, heappushpop, nlargest, nsmallest
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, combinations_with_replacement, accumulate, count, groupby
+from queue import PriorityQueue, Queue, LifoQueue
+from functools import lru_cache
+import sys
+
+sys.setrecursionlimit(10001000)
+
+MOD = int(1e9 + 7)
+INF = int(1e20)
+INFMIN = float('-inf')
+INFMAX = float('inf')
+PI = 3.141592653589793
+direc = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+direc8 = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alps = 'abcdefghijklmnopqrstuvwxyz'
+
+def alp(i):
+    return chr(ord('a') + i % 26)  # i=0->'a', i=25->'z'
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+def end(r=-1):
+    print(r)
+    exit()
+
+# -*- coding: utf-8 -*-
+# @Author  : wheat
+# @Time    : 2023/03/23 17:36
+
+if __name__ == '__main__':
+    from urllib.error import URLError
+    from urllib.request import ProxyHandler, build_opener
+
+    proxy = '127.0.0.1:7890'
+    proxy_handler = ProxyHandler(
+        {
+            'http': 'http://' + proxy,
+            'https': 'https://' + proxy
+        }
+    )
+    opener = build_opener(proxy_handler)
+    try:
+        response = opener.open('http://httpbin.org/get')
+        print(response.read().decode('utf-8'))
+    except URLError as e:
+        print(e.reason)
